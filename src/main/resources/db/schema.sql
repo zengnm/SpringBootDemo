9245239ad004b843a8966f1bcd18d67f576a7a14
zengnianmei027148 <zengnianmei027148@gtjas.com>
2024-01-29 15:44:40 +0800

    empty

--- src/main/resources/db/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE  IF NOT EXISTS `demo_order` (
-  `id` BIGINT (20) NOT NULL AUTO_INCREMENT COMMENT '主键id',
-  `user_pin` VARCHAR (20) DEFAULT NULL COMMENT '用户pin',
-  `order_price` VARCHAR (20) DEFAULT NULL COMMENT '订单金额',
-  `created` datetime NOT NULL COMMENT '创建时间',
-  `modified` datetime NOT NULL COMMENT '修改时间',
-  PRIMARY KEY (`id`)
-);
--- src/main/resources/static/js/custom/layout.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    /** 下拉菜单移入自动展开 **/
-    $('li.dropdown').mouseover(function () {
-        $(this).addClass('open');
-    }).mouseout(function () {
-        $(this).removeClass('open');
-    });
-});
