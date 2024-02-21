c8ec88cedddf29ff46338d44dafe28a64d0b00df
zengnianmei027148 <zengnianmei027148@gtjas.com>
2024-02-21 16:53:35 +0800

    tongrds接入

--- src/main/java/com/example/infrastructure/tongrds/TongrdsConfiguration.java
+++ src/main/java/com/example/infrastructure/tongrds/TongrdsConfiguration.java
@@ -1,5 +1,8 @@
 package com.example.infrastructure.tongrds;
 
+import io.lettuce.core.ClientOptions;
+import io.lettuce.core.protocol.ProtocolVersion;
+import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -24,5 +27,15 @@ public class TongrdsConfiguration {
         redisTemplate.setValueSerializer(RedisSerializer.json());
         redisTemplate.setHashValueSerializer(RedisSerializer.json());
         return redisTemplate;
+    }
+
+    /**
+     * fix
+     * https://github.com/lettuce-io/lettuce-core/issues/1201
+     * https://github.com/lettuce-io/lettuce-core/issues/1543
+     */
+    @Bean
+    public LettuceClientConfigurationBuilderCustomizer redisBuilderCustomizer() {
+        return builder -> builder.clientOptions(ClientOptions.builder().protocolVersion(ProtocolVersion.RESP2).build());
     }
 }
