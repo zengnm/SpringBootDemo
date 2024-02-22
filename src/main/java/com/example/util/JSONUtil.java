82a71833691fcc6a3e75dee5aa70f8ee575f9c56
zengnianmei027148 <zengnianmei027148@gtjas.com>
2024-02-22 17:42:50 +0800

    JSONUtil

--- src/main/java/com/example/util/JSONUtil.java
+++ src/main/java/com/example/util/JSONUtil.java
@@ -7,8 +7,6 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -22,8 +20,8 @@ import java.util.Map;
  * @author zengnianmei
  */
 public class JSONUtil {
-    private final static Logger LOG = LoggerFactory.getLogger(JSONUtil.class);
     private static final ObjectMapper objectMapper = new ObjectMapper();
+
     static {
         // 兼容单引号，但单引号不属于json标准不建议使用
         objectMapper.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
@@ -32,52 +30,43 @@ public class JSONUtil {
         // 反序列化时，不存在相匹配属性的兼容处理
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
     }
+
     private JSONUtil() {
     }
 
     public static String toJSONString(Object o) {
-        String jsonStr = "";
         try {
-            jsonStr = objectMapper.writeValueAsString(o);
+            return objectMapper.writeValueAsString(o);
         } catch (JsonProcessingException e) {
-            LOG.error("toJSONString error:", e);
+            throw new RuntimeException(e);
         }
-        return jsonStr;
     }
 
     public static <T> T parseObject(String json, Class<T> clazz) {
         try {
             return objectMapper.readValue(json, clazz);
         } catch (IOException e) {
-            LOG.error("parseObject exception, json=" + json , e);
+            throw new RuntimeException(e);
         }
-        return null;
     }
 
-    public static <T> List<T> parseList(String jsonArray, Class<T> clazz){
-        if (null == jsonArray || jsonArray.isEmpty() || null == clazz) {
-            return new ArrayList<>();
-        }
+    public static <T> List<T> parseList(String jsonArray, Class<T> clazz) {
         try {
             JavaType javaType = objectMapper.getTypeFactory().constructCollectionType(ArrayList.class, clazz);
             return objectMapper.readValue(jsonArray, javaType);
         } catch (IOException e) {
-            LOG.error("parseList exception, jsonArray=" + jsonArray, e);
+            throw new RuntimeException(e);
         }
-        return new ArrayList<>();
     }
 
     public static <T> Map<String, T> parseMap(String json, Class<T> clazz) {
-        if (null == json || json.isEmpty() || null == clazz) {
-            return new HashMap<>();
-        }
         try {
-            JavaType javaType = objectMapper.getTypeFactory().constructParametricType(HashMap.class,String.class, clazz);
+            JavaType javaType = objectMapper.getTypeFactory().constructParametricType(HashMap.class, String.class,
+                    clazz);
             return objectMapper.readValue(json, javaType);
         } catch (IOException e) {
-            LOG.error("parseMap exception, json=" + json, e);
+            throw new RuntimeException(e);
         }
-        return new HashMap<>();
     }
 
     public static Map<String, Object> parseMap(String json) {
@@ -85,14 +74,35 @@ public class JSONUtil {
     }
 
     public static List<Map<String, Object>> parseListMap(String json) {
-        if (null == json || json.isEmpty()) {
-            return new ArrayList<>();
-        }
         try {
-            return objectMapper.readValue(json, new TypeReference<List<Map<String, Object>>>(){});
+            return objectMapper.readValue(json, new TypeReference<>() {
+            });
         } catch (IOException e) {
-            LOG.error("parseListMap exception, json=" + json, e);
+            throw new RuntimeException(e);
         }
-        return new ArrayList<>();
+    }
+
+    public static void main(String[] args) {
+        record Pojo(Long id, String name) {
+        }
+        Pojo p1 = new Pojo(1L, "111");
+        String json = JSONUtil.toJSONString(p1);
+        Pojo o1 = JSONUtil.parseObject(json, Pojo.class);
+        System.out.println(o1);
+
+        Pojo p2 = new Pojo(2L, "222");
+        json = JSONUtil.toJSONString(List.of(p1, p2));
+        List<Pojo> pojos = JSONUtil.parseList(json, Pojo.class);
+        System.out.println(pojos);
+
+        Map<String, Integer> map = Map.of("key1", 1, "key2", 2);
+        json = JSONUtil.toJSONString(map);
+        Map<String, Object> stringObjectMap = JSONUtil.parseMap(json);
+        System.out.println(stringObjectMap);
+
+        List<Map<String, Integer>> listMap = List.of(map, Map.of("key3", 3));
+        json = JSONUtil.toJSONString(listMap);
+        List<Map<String, Object>> maps = JSONUtil.parseListMap(json);
+        System.out.println(maps);
     }
 }
