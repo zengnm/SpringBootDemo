ca658d1ded8ab7d10572844577d39f2f0ed3d7d2
zengnianmei027148 <zengnianmei027148@gtjas.com>
2024-07-30 10:02:02 +0800

    验证producer自动关闭和创建

--- src/main/java/com/example/SpringBootDemoApplication.java
+++ src/main/java/com/example/SpringBootDemoApplication.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import org.apache.pulsar.client.api.PulsarClientException;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,8 +8,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.pulsar.annotation.PulsarListener;
 import org.springframework.pulsar.core.PulsarTemplate;
 
+import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 @SpringBootApplication
 public class SpringBootDemoApplication {
+    private final static ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDemoApplication.class, args);
@@ -16,11 +23,18 @@ public class SpringBootDemoApplication {
 
     @Bean
     ApplicationRunner runner(PulsarTemplate<String> pulsarTemplate) {
-        return (args) -> pulsarTemplate.send("persistent://notice/test/test_simple_topic", "Hello Pulsar World!");
+        return (args) -> executor.scheduleAtFixedRate(() -> {
+            try {
+                pulsarTemplate.send("persistent://notice/test/test_simple_topic",
+                        "Hello Pulsar World!"+new Random().nextInt());
+            } catch (PulsarClientException e) {
+                throw new RuntimeException(e);
+            }
+        }, 3, 90, TimeUnit.SECONDS);
     }
 
     @PulsarListener(subscriptionName = "hello-pulsar-sub", topics = "persistent://notice/test/test_simple_topic")
     void listen(String message) {
-        System.out.println("Message Received: " + message);
+        System.out.println("Message Received: "+message);
     }
 }
